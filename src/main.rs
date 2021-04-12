f2a04e3203342fceb277ccd23daf89ea81559c0c
PutterBeanut <christian.bannoura@gmail.com>
2021-04-11 23:01:28 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use glfw::Context;
 fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
 
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
     glfw.window_hint(glfw::WindowHint::ContextVersion(4, 3));
     glfw.window_hint(glfw::WindowHint::Samples(Some(4)));
 
